ef91820349180f86f9635ffefe7e4a802e285ca8
summer brochtrup & perry eising <student@epicodus.com>
2015-08-25 16:22:22 -0700

    add integration tests

--- src/test/java/IntegrationTest.java
+++ src/test/java/IntegrationTest.java
@@ -1,6 +1,7 @@
 import org.fluentlenium.adapter.FluentTest;
 import java.util.ArrayList;
 import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
@@ -14,6 +15,9 @@ public class IntegrationTest extends FluentTest {
     return webDriver;
   }
 
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
   @ClassRule
   public static ServerRule server = new ServerRule();
 
@@ -22,5 +26,47 @@ public class IntegrationTest extends FluentTest {
     goTo("http://localhost:4567/");
     assertThat(pageSource()).contains("To Do List");
   }
+
+  @Test
+  public void displaysNewCategoryButton() {
+    goTo("http://localhost:4567/");
+    fill("#category").with("Java Class");
+    submit(".btn");
+    assertThat(pageSource()).contains("Java Class");
+  }
+
+  @Test
+  public void displaysCategoryTaskAreaOnCategoryButtonClick() {
+    Category testCategory = new Category("Java Class");
+    testCategory.save();
+    String categoryPath = String.format("http://localhost:4567/tasks/%d?categoryId=%d", testCategory.getId(), testCategory.getId());
+    goTo(categoryPath);
+    assertThat(pageSource()).contains("Tasks in Java Class Category");
+  }
+
+  @Test
+  public void newTaskDisplaysInTaskArea() {
+    Category testCategory = new Category("Java Class");
+    testCategory.save();
+    Task testTask = new Task("Watch video", testCategory.getId());
+    testTask.save();
+    String categoryPath = String.format("http://localhost:4567/tasks/%d?categoryId=%d&description=%s", testCategory.getId(), testCategory.getId(), testTask.getDescription());
+    goTo(categoryPath);
+    assertThat(pageSource()).contains("Watch video");
+  }
+
+  @Test
+  public void editedTaskDisplaysCorrectly(){
+    Category testCategory = new Category("Java Class");
+    testCategory.save();
+    Task testTask = new Task("Watch video", testCategory.getId());
+    testTask.save();
+    testTask.editTask(testTask.getId(), "Watch Treehouse");
+    String categoryPath = String.format("http://localhost:4567/category/%d/edit/%d?description=%s", testCategory.getId(), testTask.getId(), testTask.getDescription());
+    goTo(categoryPath);
+    assertThat(pageSource()).contains("Watch Treehouse");
+
+  }
+
 
 }
