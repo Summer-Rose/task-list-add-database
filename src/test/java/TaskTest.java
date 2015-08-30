73916cd693bed2d6bbac4559c7275a7a0c8fcdf7
Summer <sbrochtrup@gmail.com>
2015-08-30 14:42:16 -0700

    Remove categoryid

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -14,21 +14,21 @@ public class TaskTest {
 
   @Test
   public void equals_returnsTrueIfDescriptionsAreTheSame() {
-    Task firstTask = new Task("mow the lawn", 1);
-    Task secondTask = new Task("mow the lawn", 1);
+    Task firstTask = new Task("mow the lawn");
+    Task secondTask = new Task("mow the lawn");
     assertTrue(firstTask.equals(secondTask));
   }
 
   @Test
-  public void save_returnsTrueIfDescriptionsAreTheSame() {
-    Task myTask = new Task("mow the lawn", 1);
+  public void save_savesObjectIntoDatabase() {
+    Task myTask = new Task("mow the lawn");
     myTask.save();
     assertTrue(Task.all().get(0).equals(myTask));
   }
 
   @Test
   public void save_assignsIdToObject() {
-    Task myTask = new Task("mow the lawn", 1);
+    Task myTask = new Task("mow the lawn");
     myTask.save();
     Task savedTask = Task.all().get(0);
     assertEquals(myTask.getId(), savedTask.getId());
@@ -36,41 +36,41 @@ public class TaskTest {
 
   @Test
   public void find_findsTaskInDatabase_true() {
-    Task myTask = new Task("mow the lawn", 1);
+    Task myTask = new Task("mow the lawn");
     myTask.save();
     Task savedTask = Task.find(myTask.getId());
     assertTrue(myTask.equals(savedTask));
   }
 
-  @Test
-  public void save_savesCategoryIdIntoDB_true() {
-    Category myCategory = new Category("Household chores");
-    myCategory.save();
-    Task myTask = new Task("mow the lawn", myCategory.getId());
-    myTask.save();
-    Task savedTask = Task.find(myTask.getId());
-    assertEquals(savedTask.getCategoryId(), myCategory.getId());
-  }
+  // @Test
+  // public void save_savesCategoryIdIntoDB_true() {
+  //   Category myCategory = new Category("Household chores");
+  //   myCategory.save();
+  //   Task myTask = new Task("mow the lawn", myCategory.getId());
+  //   myTask.save();
+  //   Task savedTask = Task.find(myTask.getId());
+  //   assertEquals(savedTask.getCategoryId(), myCategory.getId());
+  // }
 
-  @Test
-  public void remove_removesTaskById_true() {
-    Category myCategory = new Category("Household chores");
-    myCategory.save();
-    Task myTask = new Task("feed fish", myCategory.getId());
-    myTask.save();
-    Task.removeTaskById(myTask.getId());
-    assertTrue(Task.all().size() == 0);
-  }
+  // @Test
+  // public void remove_removesTaskById_true() {
+  //   Category myCategory = new Category("Household chores");
+  //   myCategory.save();
+  //   Task myTask = new Task("feed fish", myCategory.getId());
+  //   myTask.save();
+  //   Task.removeTaskById(myTask.getId());
+  //   assertTrue(Task.all().size() == 0);
+  // }
 
-  @Test
-  public void edit_newTaskDescription() {
-    Category myCategory = new Category("Household chores");
-    myCategory.save();
-    Task myTask = new Task("feed fish", myCategory.getId());
-    myTask.save();
-    Task editTask = new Task("feed cat", myCategory.getId());
-    myTask.editTask(editTask.getId(), editTask.getDescription());
-    assertTrue(editTask.getDescription() == "feed cat");
-  }
+  // @Test
+  // public void edit_newTaskDescription() {
+  //   Category myCategory = new Category("Household chores");
+  //   myCategory.save();
+  //   Task myTask = new Task("feed fish", myCategory.getId());
+  //   myTask.save();
+  //   Task editTask = new Task("feed cat", myCategory.getId());
+  //   myTask.editTask(editTask.getId(), editTask.getDescription());
+  //   assertTrue(editTask.getDescription() == "feed cat");
+  // }
 
 }
