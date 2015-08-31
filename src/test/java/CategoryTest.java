25f5c734002a725e0f68196828e5e8dbb3f4356a
Summer <sbrochtrup@gmail.com>
2015-08-30 18:55:22 -0700

    Add unit tests for delete and getTasks methods

--- src/test/java/CategoryTest.java
+++ src/test/java/CategoryTest.java
@@ -1,6 +1,7 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.Arrays;
+import java.util.List;
 
 public class CategoryTest {
 
@@ -35,15 +36,48 @@ public class CategoryTest {
   }
 
   @Test
-  public void getTasks_retreiveAllTasksFromDatabase_tasksList() {
+  public void addTask_addsTaskToCategory() {
     Category myCategory = new Category("Household chores");
     myCategory.save();
-    Task firstTask = new Task("mow the lawn", myCategory.getId());
-    firstTask.save();
-    Task secondTask = new Task("do the dishes", myCategory.getId());
-    secondTask.save();
-    Task[] tasks = new Task[] { firstTask, secondTask };
-    assertTrue(myCategory.getTasks().containsAll(Arrays.asList(tasks)));
-
+    Task myTask = new Task("Mow the lawn");
+    myTask.save();
+    myCategory.addTask(myTask);
+    Task savedTask = myCategory.getTasks().get(0);
+    assertTrue(myTask.equals(savedTask));
   }
+
+  @Test
+  public void getTasks_returnsAllTasks_ArrayList() {
+    Category myCategory = new Category("Household chores");
+    myCategory.save();
+    Task myTask = new Task("Mow the lawn");
+    myTask.save();
+    myCategory.addTask(myTask);
+    List savedTasks = myCategory.getTasks();
+    assertEquals(savedTasks.size(), 1);
+  }
+
+  @Test
+  public void delete_deletesAllTasksAndListAssociations() {
+    Category myCategory = new Category("Household chores");
+    myCategory.save();
+    Task myTask = new Task("Mow the lawn");
+    myTask.save();
+    myCategory.addTask(myTask);
+    myCategory.delete();
+    assertEquals(myTask.getCategories().size(), 0);
+  }
+
+  // @Test
+  // public void getTasks_retreiveAllTasksFromDatabase_tasksList() {
+  //   Category myCategory = new Category("Household chores");
+  //   myCategory.save();
+  //   Task firstTask = new Task("mow the lawn");
+  //   firstTask.save();
+  //   Task secondTask = new Task("do the dishes");
+  //   secondTask.save();
+  //   Task[] tasks = new Task[] { firstTask, secondTask };
+  //   assertTrue(myCategory.getTasks().containsAll(Arrays.asList(tasks)));
+
+  // }
 }
