b1ef43971b8d61d633f01261a647954ea1e949ed
Summer <sbrochtrup@gmail.com>
2015-08-30 18:54:33 -0700

    Add delete and getCategories methods

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,5 +1,7 @@
 import java.util.List;
 import org.sql2o.*;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 public class Task {
   private int id;
@@ -78,6 +80,47 @@ public class Task {
         .addParameter("id", id)
         .executeAndFetchFirst(Task.class);
       return task;
+    }
+  }
+
+  public void addCategory(Category category) {
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO categories_tasks (category_id, task_id) VALUES (:category_id, :task_id)";
+      con.createQuery(sql)
+        .addParameter("category_id", category.getId())
+        .addParameter("task_id", this.getId())
+        .executeUpdate();
+    }
+  }
+
+  public ArrayList<Category> getCategories() {
+    try (Connection con = DB.sql2o.open()) {
+      String sql = "SELECT category_id FROM categories_tasks WHERE task_id = :task_id";
+      List<Integer> categoryIds = con.createQuery(sql)
+        .addParameter("task_id", this.getId())
+        .executeAndFetch(Integer.class);
+      ArrayList<Category> categories = new ArrayList<Category>();
+      for (Integer categoryId : categoryIds) {
+        String taskQuery = "SELECT * FROM categories WHERE id = :categoryId";
+        Category category = con.createQuery(taskQuery)
+          .addParameter("categoryId", categoryId)
+          .executeAndFetchFirst(Category.class);
+        categories.add(category);
+      }
+      return categories;
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String deleteQuery = "DELETE FROM tasks WHERE id=:id";
+      con.createQuery(deleteQuery)
+        .addParameter("id", id)
+        .executeUpdate();
+      String joinDeleteQuery = "DELETE FROM categories_tasks WHERE task_id=:taskId";
+      con.createQuery(joinDeleteQuery)
+        .addParameter("taskId", this.getId())
+        .executeUpdate();
     }
   }
 }
